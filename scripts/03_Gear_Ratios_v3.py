34697c5dc3f0302132269d017560b9eb63add1b2
Kathryn Potten <kathrynpotten@users.noreply.github.com>
2023-12-05 14:57:04 +0000

    3-1 Gear Ratios version 3

---
+++ scripts/03_Gear_Ratios_v3.py
@@ -0,0 +1,82 @@
+import re
+from collections import Counter
+
+
+test_data = """467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598.."""
+
+test_result = 4361
+
+
+class Schematic:
+    def __init__(self, input):
+        self.input = input
+        self.scheme = re.sub(r"[^a-zA-Z0-9 \n\.]", "*", input).splitlines()
+        self.symbols = []
+        self.adjacent = []
+
+    def __repr__(self):
+        return repr(self.input)
+
+    def __str__(self):
+        return "\n".join(line for line in self.scheme)
+
+    def position_of_symbols(self):
+        for index, line in enumerate(self.scheme):
+            symbols_positions = re.finditer(r"\*", line)
+            self.symbols.extend([(index, pos.span()[0]) for pos in symbols_positions])
+
+    def adjacency(self):
+        adjacency_conditions = [
+            (-1, 0),
+            (1, 0),
+            (0, 1),
+            (0, -1),
+            (-1, -1),
+            (-1, 1),
+            (1, 1),
+            (1, -1),
+        ]
+        for symbol in self.symbols:
+            adjacenct_positions = [
+                tuple(x + y for x, y in zip(symbol, condition))
+                for condition in adjacency_conditions
+            ]
+            self.adjacent.extend(adjacenct_positions)
+        self.adjacent.sort()
+
+    def sum_engine_parts(self):
+        self.position_of_symbols()
+        self.adjacency()
+        sum = 0
+        for row_index, line in enumerate(self.scheme):
+            digits = re.finditer(r"\d+", line)
+            column_indices = []
+            for pos in digits:
+                column_indices = [x for x in range(pos.span()[0], pos.span()[1])]
+                if any(
+                    (row_index, col_index) in self.adjacent
+                    for col_index in column_indices
+                ):
+                    sum += int(pos.group())
+        return sum
+
+
+test_schematic = Schematic(test_data)
+assert test_schematic.sum_engine_parts() == test_result
+
+
+with open("../input_data/03_Gear_Ratios.txt", "r", encoding="utf-8") as file:
+    input = file.read().strip()
+
+answer_schematic = Schematic(input)
+answer_1 = answer_schematic.sum_engine_parts()
+print(answer_1)
