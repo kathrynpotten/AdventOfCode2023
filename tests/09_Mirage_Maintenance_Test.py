2c89e057e76a9d6fcf62ec76a2758f9f86963f15
Kathryn Potten <kathrynpotten@users.noreply.github.com>
2023-12-18 16:39:32 +0000

    9-1 Mirage Maintenance

---
+++ tests/09_Mirage_Maintenance_Test.py
@@ -0,0 +1,36 @@
+import unittest
+import Mirage_Maintenance_Testing as mirage
+
+
+class Test(unittest.TestCase):
+    def test_seq_of_differences(self):
+        value = [0, 3, 6, 9, 12, 15]
+        history = mirage.History(value)
+        result = history.sequence_of_differences()
+        expected_result = [3, 3, 3, 3, 3]
+        self.assertEqual(result, expected_result)
+
+    def test_difference_iter(self):
+        value = [0, 3, 6, 9, 12, 15]
+        history = mirage.History(value)
+        result = history.differences_iter()
+        expected_result = [value, [3, 3, 3, 3, 3], [0, 0, 0, 0]]
+        self.assertEqual(result, expected_result)
+
+    def test_next_value(self):
+        value = [0, 3, 6, 9, 12, 15]
+        history = mirage.History(value)
+        history.differences_iter()
+        result = history.next_value()
+        self.assertEqual(result, 18)
+
+    def test_sum(self):
+        test_data = (
+            """0 3 6 9 12 15\n1 3 6 10 15 21\n10 13 16 21 30 45""".strip().split("\n")
+        )
+        test_result = 114
+        self.assertEqual(mirage.sum_of_extrapolated(test_data), test_result)
+
+
+if __name__ == "__main__":
+    unittest.main()
