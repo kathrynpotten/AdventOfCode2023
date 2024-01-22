59c009507c8a63c5172da5227deb38199e17c100
Kathryn Potten <kathrynpotten@users.noreply.github.com>
2024-01-22 18:00:30 +0000

    12-1 Hot Springs

--- scripts/12_Hot_Springs/Hot_Springs.py
+++ scripts/12_Hot_Springs/Hot_Springs.py
@@ -26,51 +26,37 @@ def replace_springs(spring_string):
 def check_possible_spring_placement(total_springs, row, index, grouping, order):
     index_count = index
     for new_index, next_item in enumerate(row[index + 1 :]):
-        # print(f"total springs is {total_springs}")
-        # print(f"current row is {row}, next item is {next_item}")
-        # print(f"index count is {index_count}")
         if total_springs == grouping:
             row[index] = grouping
-            #   print(index_count)
             if index_count + 1 < len(row) and row[index_count + 1] == "?":
                 row[index_count + 1] = "."
             row = row[: index + 1] + row[index_count + 1 :]
             order = order[1:]
-            #  print(row)
             break
         index_count += 1
         if next_item == ".":
-            # print("it's a dot!")
-            # print(new_index)
-            row[index] = "."
+            if type(row[index]) == str:
+                row[index] = "."
             for x in range(new_index):
                 if type(row[index + x]) == str:
                     row[index + x] = "."
-            # print(row)
             break
         elif type(next_item) == int:
             if next_item + total_springs == grouping:
-                #   print("correct Number!")
                 row[index] = grouping
-                #  print(row, index_count)
                 if index_count + 1 < len(row) and row[index_count + 1] == "?":
                     row[index_count + 1] = "."
                 row = row[: index + 1] + row[index_count + 1 :]
-                #     print(row)
                 order = order[1:]
                 break
             elif next_item + total_springs > grouping:
                 row[index] = "."
-                # for x in range(new_index + 1):
-                #    if type(row[index + x]) == str:
-                #        row[index + x] = "."
                 break
             elif next_item + total_springs < grouping:
                 total_springs += next_item
         elif next_item == "?":
             total_springs += 1
             if new_index == len(row[index + 1 :]) - 1:
-                #    print("we've reached the end!")
                 if total_springs == grouping:
                     row[index] = grouping
                     row = row[: index + 1]
@@ -79,7 +65,8 @@ def check_possible_spring_placement(total_springs, row, index, grouping, order):
     return row, order
 
 
-def possible_configurations(input_row, order):
+def possible_configurations(input_row, order, original_row):
+    possible = False
     spring_row = input_row.copy()
     original_order = order.copy()
     for i in range(len(spring_row)):
@@ -88,15 +75,13 @@ def possible_configurations(input_row, order):
         else:
             index = i
             item = spring_row[i]
-            spring_sets = [item for item in spring_row if type(item) == int]
 
+            spring_sets = [item for item in spring_row if type(item) == int]
             if spring_sets == original_order:
                 spring_row = [item if item != "?" else "." for item in spring_row]
                 order = []
                 break
 
-            #  print(index, item)
-            #  print(f"current row is {spring_row}")
             if len(order) == 0:
                 for i in range(index, len(spring_row)):
                     if spring_row[i] == "?":
@@ -104,15 +89,12 @@ def possible_configurations(input_row, order):
                 break
             elif item == "?":
                 grouping = order[0]
-                #     print(f"grouping is {grouping}")
                 if index + grouping > len(spring_row) and all(
                     type(item) == str for item in spring_row[index:]
                 ):
                     break
                 if all(spring_row[index + x] == "?" for x in range(grouping)):
-                    #        print("may be possible!")
                     if index + grouping == len(spring_row):
-                        #           print("we reach the end!")
                         spring_row[index] = grouping
                         spring_row = (
                             spring_row[: index + 1] + spring_row[index + grouping :]
@@ -127,7 +109,6 @@ def possible_configurations(input_row, order):
                         )
                         order = order[1:]
                     else:
-                        #          print("integer!")
                         spring_row[index] = "."
 
                 else:
@@ -137,7 +118,6 @@ def possible_configurations(input_row, order):
                     )
             elif type(item) == int:
                 grouping = order[0]
-                # print(f"grouping is {grouping}")
                 if item == grouping:
                     if index + 1 < len(spring_row) and spring_row[index + 1] == "?":
                         spring_row[index + 1] = "."
@@ -146,95 +126,92 @@ def possible_configurations(input_row, order):
                     if index + 1 < len(spring_row) and spring_row[index + 1] == "?":
                         spring_row[index + 1] = "."
                 elif item < grouping:
-                    #    print("less than grouping")
                     if index + 1 >= len(spring_row):
                         break
                     elif spring_row[index + 1] == ".":
-                        break
+                        continue
                     elif spring_row[index + 1] == "?":
-                        #       print("still possible!")
                         total_springs = item
                         spring_row, order = check_possible_spring_placement(
                             total_springs, spring_row, index, grouping, order
                         )
 
-    # this section needs sorting ... we need to compare to the equivalent spot on the input, not the output
     spring_sets = [item for item in spring_row if type(item) == int]
-    # print(f"sets are {spring_sets}, original_order is {original_order}")
     if spring_sets == original_order:
-        return spring_row, order
-    elif spring_sets != original_order and spring_sets:
-        # print("problem!")
+        possible = True
+        return (spring_row, order, possible)
+    elif (
+        spring_sets != original_order
+        and len(spring_sets) > len(original_order)
+        and spring_sets
+    ):
         for spring_set in spring_sets:
             new_input_row = input_row.copy()
-            # print(spring_set)
-            # print(spring_row)
             initial_index = spring_row.index(spring_set)
             current_space_count = initial_index + sum(
                 [item - 1 for item in spring_row[:initial_index] if type(item) == int]
             )
             input_space_count = 0
-            for index, item in enumerate(input_row):
+            for index, item in enumerate(original_row):
                 if input_space_count == current_space_count:
                     final_index = index
                 if type(item) == int:
                     input_space_count += item
                 else:
                     input_space_count += 1
-            # print(final_index)
-            # print(f"input value is {input_row[final_index]}")
             if (
-                input_row[final_index] != spring_set
-                and type(input_row[final_index]) != int
+                original_row[final_index] != spring_set
+                and type(original_row[final_index]) != int
             ):
                 new_input_row[final_index] = "."
-                # print(new_input_row)
-                spring_row, order = possible_configurations(
-                    new_input_row, original_order
+                output_row, order, possible = possible_configurations(
+                    new_input_row, original_order, original_row
                 )
-        # print("completed loop")
-
-    return spring_row, order
-
-
-def count_possible_configurations(spring_row, order):
-    possibilities = 0
-    seen = []
-    possibilities += test_possible_configurations(
-        spring_row, order, possibilities, seen
-    )[0]
-    # print(possibilities)
-    error = False
-    if possibilities == 0:
-        error = True
-    return possibilities, error
+                if possible:
+                    spring_row = output_row
+                    break
+    return spring_row, order, possible
 
 
-def test_arrangement(input_row, order):
-    output_arrangement, order_residual = possible_configurations(input_row, order)
+def test_arrangement(input_row, order, original_row):
+    output_arrangement, order_residual, possible = possible_configurations(
+        input_row, order, original_row
+    )
     if not order_residual:
+        print(output_arrangement)
         set_indices_dict = {}
         for spring_set in order:
             if spring_set in set_indices_dict:
                 past_index = set_indices_dict[spring_set][0]
-                set_indices_dict[spring_set] = [
-                    past_index,
+                current_index = (
                     output_arrangement[past_index + 1 :].index(spring_set)
                     + past_index
-                    + 1,
+                    + 1
+                )
+                set_indices_dict[spring_set] = [
+                    past_index,
+                    current_index,
                 ]
             else:
-                set_indices_dict[spring_set] = [output_arrangement.index(spring_set)]
+                current_index = output_arrangement.index(spring_set)
+                set_indices_dict[spring_set] = [current_index]
+        for spring_set in set_indices_dict.keys():
+            set_index = set_indices_dict[spring_set]
+            for index in set_index:
+                if original_row[index] == spring_set:
+                    set_indices_dict[spring_set] = [i for i in set_index if i != index]
+
         set_indices = sum(set_indices_dict.values(), [])
         return set_indices
     else:
         return False
 
 
-def test_possible_configurations(spring_row, order, possibilities, seen):
-    set_indices = test_arrangement(spring_row, order)
+def test_possible_configurations(spring_row, order, possibilities, seen, original_row):
+    set_indices = test_arrangement(spring_row, order, original_row)
     if set_indices:
         if set_indices not in seen:
+            print(set_indices)
             seen.append(set_indices)
             possibilities += 1
             for set_index in set_indices:
@@ -246,12 +223,25 @@ def test_possible_configurations(spring_row, order, possibilities, seen):
                         for i in range(x):
                             if spring_row[set_index + i] == "?":
                                 input_row[set_index + i] = "."
-                            # print(f"input row is {input_row}")
                             possibilities, seen = test_possible_configurations(
-                                input_row, order, possibilities, seen
+                                input_row, order, possibilities, seen, original_row
                             )
 
     return possibilities, seen
+
+
+def count_possible_configurations(spring_row, order):
+    possibilities = 0
+    seen = []
+    original_row = spring_row.copy()
+    possibilities += test_possible_configurations(
+        spring_row, order, possibilities, seen, original_row
+    )[0]
+    # print(possibilities)
+    error = False
+    if possibilities == 0:
+        error = True
+    return possibilities, error
 
 
 def sum_of_arrangements(input_strings):
@@ -260,11 +250,11 @@ def sum_of_arrangements(input_strings):
     for num, springs in enumerate(input_strings):
         spring_string, order = parse_line(springs)
         spring_row = replace_springs(spring_string)
-        if num == 65:
-            print(f"{num}: {spring_string}, {spring_row}, {order}")
+        # if num == 19:
+        #    print(f"{num}: {spring_string}, {spring_row}, {order}")
 
-        # print("\n")
-        # print(f"{num}: {spring_string}, {spring_row}, {order}")
+        print("\n")
+        print(f"{num}: {spring_string}, {spring_row}, {order}")
         possible, error = count_possible_configurations(spring_row, order)
         sum += possible
         if error:
@@ -277,7 +267,11 @@ if __name__ == "__main__":
     with open("../../input_data/12_Hot_Springs.txt", "r", encoding="utf-8") as file:
         input_data = file.read().strip().split("\n")
 
+    input_data = input_data[:163] + input_data[164:]
+
     answer_1 = sum_of_arrangements(input_data)
     print(answer_1)
 
 # too low 4429
+
+# stuck on #163, error #792
--- scripts/12_Hot_Springs/Hot_Springs_Test.py
+++ scripts/12_Hot_Springs/Hot_Springs_Test.py
@@ -12,13 +12,13 @@ test_data = """???.### 1,1,3
 
 
 class Test(unittest.TestCase):
-    def test_parse_line(self):
+    """def test_parse_line(self):
         test_string = "???.### 1,1,3"
         result = springs.parse_line(test_string)
         expected_result = "???.###", [1, 1, 3]
         self.assertEqual(result, expected_result)
 
-    """def test_replace_springs(self):
+    def test_replace_springs(self):
         test_string_1 = "???.### 1,1,3"
         spring_string_1 = springs.parse_line(test_string_1)[0]
         result_1 = springs.replace_springs(spring_string_1)
@@ -40,13 +40,17 @@ class Test(unittest.TestCase):
         test_string_1 = "???.### 1,1,3"
         spring_string_1, order_1 = springs.parse_line(test_string_1)
         spring_row_1 = springs.replace_springs(spring_string_1)
-        result_1 = springs.possible_configurations(spring_row_1, order_1)[0]
+        result_1 = springs.possible_configurations(spring_row_1, order_1, spring_row_1)[
+            0
+        ]
         expected_result_1 = [1, ".", 1, ".", 3]
 
         test_string_2 = "?###???????? 3,2,1"
         spring_string_2, order_2 = springs.parse_line(test_string_2)
         spring_row_2 = springs.replace_springs(spring_string_2)
-        result_2 = springs.possible_configurations(spring_row_2, order_2)[0]
+        result_2 = springs.possible_configurations(spring_row_2, order_2, spring_row_2)[
+            0
+        ]
         expected_result_2 = [".", 3, ".", 2, ".", 1, ".", ".", "."]
 
         spring_row_3, order_3 = ["?", 3, "?", ".", ".", ".", "?", "?", "?", "?"], [
@@ -54,7 +58,9 @@ class Test(unittest.TestCase):
             2,
             1,
         ]
-        result_3 = springs.possible_configurations(spring_row_3, order_3)[0]
+        result_3 = springs.possible_configurations(spring_row_3, order_3, spring_row_3)[
+            0
+        ]
         expected_result_3 = [".", 3, ".", ".", ".", ".", 2, ".", 1]
         self.assertEqual(
             (result_1, result_2, result_3),
@@ -65,19 +71,25 @@ class Test(unittest.TestCase):
         test_string_1 = ".??..??...?##. 1,1,3"
         spring_string_1, order_1 = springs.parse_line(test_string_1)
         spring_row_1 = springs.replace_springs(spring_string_1)
-        result_1 = springs.possible_configurations(spring_row_1, order_1)[0]
+        result_1 = springs.possible_configurations(spring_row_1, order_1, spring_row_1)[
+            0
+        ]
         expected_result_1 = [".", 1, ".", ".", ".", 1, ".", ".", ".", ".", 3, "."]
 
         test_string_2 = "????##?#??. 6,1,1"
         spring_string_2, order_2 = springs.parse_line(test_string_2)
         spring_row_2 = springs.replace_springs(spring_string_2)
-        result_2 = springs.possible_configurations(spring_row_2, order_2)[0]
+        result_2 = springs.possible_configurations(spring_row_2, order_2, spring_row_2)[
+            0
+        ]
         expected_result_2 = [6, ".", 1, ".", 1, "."]
 
         test_string_3 = "##?#??#.?# 7,1"
         spring_string_3, order_3 = springs.parse_line(test_string_3)
         spring_row_3 = springs.replace_springs(spring_string_3)
-        result_3 = springs.possible_configurations(spring_row_3, order_3)[0]
+        result_3 = springs.possible_configurations(spring_row_3, order_3, spring_row_3)[
+            0
+        ]
         expected_result_3 = [7, ".", ".", 1]
         self.assertEqual(
             (result_1, result_2, result_3),
@@ -88,19 +100,25 @@ class Test(unittest.TestCase):
         test_string_1 = "?#?#?#?#?#?#?#? 1,3,1,6"
         spring_string_1, order_1 = springs.parse_line(test_string_1)
         spring_row_1 = springs.replace_springs(spring_string_1)
-        result_1 = springs.possible_configurations(spring_row_1, order_1)[0]
+        result_1 = springs.possible_configurations(spring_row_1, order_1, spring_row_1)[
+            0
+        ]
         expected_result_1 = [".", 1, ".", 3, ".", 1, ".", 6]
 
         test_string_2 = "?##?##???? 7,1"
         spring_string_2, order_2 = springs.parse_line(test_string_2)
         spring_row_2 = springs.replace_springs(spring_string_2)
-        result_2 = springs.possible_configurations(spring_row_2, order_2)[0]
+        result_2 = springs.possible_configurations(spring_row_2, order_2, spring_row_2)[
+            0
+        ]
         expected_result_2 = [7, ".", 1, "."]
 
         test_string_3 = "?#??#.????..?##? 1,2,3,4"
         spring_string_3, order_3 = springs.parse_line(test_string_3)
         spring_row_3 = springs.replace_springs(spring_string_3)
-        result_3 = springs.possible_configurations(spring_row_3, order_3)[0]
+        result_3 = springs.possible_configurations(spring_row_3, order_3, spring_row_3)[
+            0
+        ]
         expected_result_3 = [".", 1, ".", 2, ".", 3, ".", ".", ".", 4]
 
         self.assertEqual(
@@ -112,49 +130,80 @@ class Test(unittest.TestCase):
         test_string_1 = "???????##?.??? 7,2"
         spring_string_1, order_1 = springs.parse_line(test_string_1)
         spring_row_1 = springs.replace_springs(spring_string_1)
-        result_1 = springs.possible_configurations(spring_row_1, order_1)[0]
+        result_1 = springs.possible_configurations(spring_row_1, order_1, spring_row_1)[
+            0
+        ]
         expected_result_1 = [".", ".", 7, ".", ".", 2, "."]
 
         test_string_2 = "?..?????#####??.??? 1,2,7,3"
         spring_string_2, order_2 = springs.parse_line(test_string_2)
         spring_row_2 = springs.replace_springs(spring_string_2)
-        result_2 = springs.possible_configurations(spring_row_2, order_2)[0]
+        result_2 = springs.possible_configurations(spring_row_2, order_2, spring_row_2)[
+            0
+        ]
         expected_result_2 = [1, ".", ".", 2, ".", 7, ".", ".", ".", 3]
 
         test_string_3 = "??????#.#?#?#?? 5,5"
         spring_string_3, order_3 = springs.parse_line(test_string_3)
         spring_row_3 = springs.replace_springs(spring_string_3)
-        result_3 = springs.possible_configurations(spring_row_3, order_3)[0]
+        result_3 = springs.possible_configurations(spring_row_3, order_3, spring_row_3)[
+            0
+        ]
         expected_result_3 = [".", ".", 5, ".", 5, ".", "."]
 
         self.assertEqual(
             (result_1, result_2, result_3),
             (expected_result_1, expected_result_2, expected_result_3),
-        )"""
+        )
 
     def test_possible_configurations_4(self):
-        # test_string_1 = "?.??#?#?.??.#.? 5,1"
-        # spring_string_1, order_1 = springs.parse_line(test_string_1)
-        # spring_row_1 = springs.replace_springs(spring_string_1)
-        # result_1 = springs.possible_configurations(spring_row_1, order_1)[0]
-        result_1 = [".", ".", 5, ".", ".", ".", ".", ".", 1, ".", "."]
+        test_string_1 = "?.??#?#?.??.#.? 5,1"
+        spring_string_1, order_1 = springs.parse_line(test_string_1)
+        spring_row_1 = springs.replace_springs(spring_string_1)
+        result_1 = springs.possible_configurations(spring_row_1, order_1, spring_row_1)[
+            0
+        ]
         expected_result_1 = [".", ".", 5, ".", ".", ".", ".", ".", 1, ".", "."]
 
         test_string_2 = "###????..#???#?? 5,1,2"
         spring_string_2, order_2 = springs.parse_line(test_string_2)
         spring_row_2 = springs.replace_springs(spring_string_2)
-        result_2 = springs.possible_configurations(spring_row_2, order_2)[0]
+        result_2 = springs.possible_configurations(spring_row_2, order_2, spring_row_2)[
+            0
+        ]
         expected_result_2 = [5, ".", ".", ".", ".", 1, ".", ".", 2, ".", "."]
 
+        test_string_3 = "?.??#????##????#.#? 1,1,4,3,1"
+        spring_string_3, order_3 = springs.parse_line(test_string_3)
+        spring_row_3 = springs.replace_springs(spring_string_3)
+        result_3 = springs.possible_configurations(spring_row_3, order_3, spring_row_3)[
+            0
+        ]
+        expected_result_3 = [".", ".", 1, ".", 1, ".", ".", 4, ".", ".", 3, ".", 1, "."]
+
         self.assertEqual(
-            (result_1, result_2),
-            (expected_result_1, expected_result_2),
+            (result_1, result_2, result_3),
+            (expected_result_1, expected_result_2, expected_result_3),
         )
 
-    """def test_impossible(self):
+    def test_possible_configurations_5(self):
+        test_string_1 = ".??#.???????#?.???.? 1,7,3"
+        spring_string_1, order_1 = springs.parse_line(test_string_1)
+        spring_row_1 = springs.replace_springs(spring_string_1)
+        result_1 = springs.possible_configurations(spring_row_1, order_1, spring_row_1)[
+            0
+        ]
+        expected_result_1 = [".", ".", ".", 1, ".", ".", 7, ".", ".", 3, ".", "."]
+
+        self.assertEqual(
+            (result_1),
+            (expected_result_1),
+        )
+
+    def test_impossible(self):
         test_row = ["?", 3, "?", ".", ".", ".", ".", ".", ".", "?"]
         order = [3, 2, 1]
-        result = springs.possible_configurations(test_row, order)[0]
+        result = springs.possible_configurations(test_row, order, test_row)[0]
         expected_result = [".", 3, ".", ".", ".", ".", ".", ".", ".", "?"]
         self.assertEqual(result, expected_result)
 
@@ -171,9 +220,82 @@ class Test(unittest.TestCase):
         result_2 = springs.count_possible_configurations(spring_row_2, order_2)[0]
         expected_result_2 = 10
 
-        self.assertEqual((result_1, result_2), (expected_result_1, expected_result_2))
+        test_string_3 = ".??..??...?##. 1,1,3"
+        spring_string_3, order_3 = springs.parse_line(test_string_3)
+        spring_row_3 = springs.replace_springs(spring_string_3)
+        result_3 = springs.count_possible_configurations(spring_row_3, order_3)[0]
+        expected_result_3 = 4
 
-    def test_sum_of_arrangements(self):
+        test_string_4 = "?.??#????##????#.#? 1,1,4,3,1"
+        spring_string_4, order_4 = springs.parse_line(test_string_4)
+        spring_row_4 = springs.replace_springs(spring_string_4)
+        result_4 = springs.count_possible_configurations(spring_row_4, order_4)[0]
+        expected_result_4 = 5
+
+        self.assertEqual(
+            (result_1, result_2, result_3, result_4),
+            (
+                expected_result_1,
+                expected_result_2,
+                expected_result_3,
+                expected_result_4,
+            ),
+        )
+
+    def test_change_to_input_bug(self):
+        test_row, order = [
+            "?",
+            ".",
+            "?",
+            "?",
+            1,
+            "?",
+            "?",
+            ".",
+            "?",
+            2,
+            "?",
+            "?",
+            "?",
+            "?",
+            1,
+            ".",
+            1,
+            "?",
+        ], [1, 1, 4, 3, 1]
+        original_row = test_row
+        result = springs.possible_configurations(test_row, order, original_row)[0]
+        expected_result = [".", ".", 1, ".", 1, ".", ".", ".", 4, ".", 3, ".", 1, "."]
+
+        self.assertEqual(result, expected_result)"""
+
+    def test_count_possibilities_2(self):
+        test_row, order = [
+            "?",
+            "?",
+            "?",
+            "?",
+            "?",
+            "?",
+            ".",
+            "?",
+            "?",
+            "?",
+            "?",
+            "?",
+            "?",
+            1,
+            "?",
+            "?",
+            1,
+            "?",
+        ], [1, 1, 1, 2, 7]
+        result = springs.count_possible_configurations(test_row, order)[0]
+        expected_result = 12
+        self.assertEqual(result, expected_result)
+        # not picking up third 1 in set_indices - why? possibilities missing are [0,2,5,7,9], [0,2,5,7,10], [0,2,5,8, 10]
+
+    """def test_sum_of_arrangements(self):
         result = springs.sum_of_arrangements(test_data)
         expected_result = 21
         self.assertEqual(result, expected_result)"""
